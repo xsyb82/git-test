82df4f1c7ce961408031d0e5ac0669fbdce94181
xsy <xsy820000@163.com>
2021-10-20 19:32:52 +0800

    push test

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello git.123123123");
         System.out.println("hot-fix master");
         System.out.println("hot-fix");
+        System.out.println("push test");
     }
 }
